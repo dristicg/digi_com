ee69777edbff5061fae37b531e833a184db54c51
dristicg <dristi.gupta.cg@gmail.com>
2025-02-09 11:01:33 +0530

    added client side file installend the necessary packeges for frontend and css framework tailwind css and shadcn and exported button from shadcn

---
+++ server/server.js
@@ -0,0 +1,36 @@
+ 
+ const express = require("express");
+ const mongoose = require("mongoose");
+ const cookieParser = require("cookie-parser");
+ const cors = require("cors");
+
+ //create a database connection -> u can also
+//create a separate file for this and then import/use that file here
+
+mongoose
+  .connect("mongodb+srv://dristigupta2305:uHW9aGhVCHi4JEMr@digicom.hvobg.mongodb.net/")
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+ const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: "http://localhost:5173/",
+        methods: ["GET", "POST", "DELETE", "PUT"],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma",
+        ],
+        credentials: true,
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
