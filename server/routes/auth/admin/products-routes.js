c16ced7e79f85850d05a268e9d2ddfec4ad102cc
dristicg <dristi.gupta.cg@gmail.com>
2025-02-21 10:52:47 +0530

    Implement image upload API with Cloudinary integration
    Added handleImageUpload function to process and upload images using Cloudinary. Fixed a module.export typo in the router file to ensure proper API functionality.

---
+++ server/routes/auth/admin/products-routes.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+
+const {
+    handleImageUpload,
+    // addProduct,
+    // editProduct,
+    // fetchAllProducts,
+    // deleteProduct,
+  } = require("../../controllers/admin/products-controller");
+
+const { upload } = require("../../helpers/cloudinary");
+const router = express.Router();
+
+router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+
+module.export = router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+// const adminOrderRouter = require("./routes/admin/order-routes");
 
 // Database Connection
 mongoose
@@ -26,5 +28,6 @@ app.use(
 
 // Routes
 app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
