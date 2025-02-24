efb811f8ade52b8437a66ac83e9388e448011f10
dristicg <dristi.gupta.cg@gmail.com>
2025-02-24 12:06:57 +0530

    added sort option

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,12 +1,81 @@
-
 import ProductFilter from "@/components/shopping-view/filter";
+// import ProductDetailsDialog from "@/components/shopping-view/product-details";
+// import ShoppingProductTile from "@/components/shopping-view/product-tile";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+// import { useToast } from "@/components/ui/use-toast";
+ import { sortOptions } from "@/config";
+// import { addToCart, fetchCartItems } from "@/store/shop/cart-slice";
+// import {
+//   fetchAllFilteredProducts,
+//    fetchProductDetails,
+// } from "@/store/shop/products-slice";
+ import { ArrowUpDownIcon } from "lucide-react";
+ import { useEffect, useState } from "react";
+ import { useDispatch, useSelector } from "react-redux";
+// import { useSearchParams } from "react-router-dom";
 
-function ShoppingListing() {
-    return ( 
+
+function ShoppingListing( { productList, productDetails }) {
+    const dispatch = useDispatch();
+    // const { productList, productDetails } = useSelector(
+    //   (state) => state.shopProducts
+    // ); due to redux this os off
+    const [sort, setSort] = useState(null);
+
+    function handleSort(value) {
+        setSort(value);
+      }
+    
+
+
+    return (
         <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6" >
-        <ProductFilter />
+            <ProductFilter />
+            <div className="bg-background w-full rounded-lg shadow-sm">
+                <div className="p-4 border-b flex items-center justify-between">
+                    <h2 className="text-lg font-extrabold">All Products</h2>
+                    <div className="flex items-center gap-3">
+                        <span className="text-muted-foreground">
+                            {productList?.length} Products
+                        </span>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    className="flex items-center gap-1"
+                                >
+                                    <ArrowUpDownIcon className="h-4 w-4" />
+                                    <span>Sort by</span>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end" className="w-[200px]">
+                                <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
+                                    {sortOptions.map((sortItem) => (
+                                        <DropdownMenuRadioItem
+                                            value={sortItem.id}
+                                            key={sortItem.id}
+                                        >
+                                            {sortItem.label}
+                                        </DropdownMenuRadioItem>
+                                    ))}
+                                </DropdownMenuRadioGroup>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
-     );
+    );
 }
 
 export default ShoppingListing;
