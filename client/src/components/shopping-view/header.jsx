f18e8df9b2b9706e37dea0abc9c3629be033bf91
dristicg <dristi.gupta.cg@gmail.com>
2025-02-24 08:17:10 +0530

    header for shopping view

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -11,6 +11,20 @@ import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
 import { useDispatch, useSelector } from "react-redux";
 import { shoppingViewHeaderMenuItems } from "@/config";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "../ui/dropdown-menu";
+  import { Avatar, AvatarFallback } from "../ui/avatar";
+  import { logoutUser } from "@/store/auth-slice";
+//   import UserCartWrapper from "./cart-wrapper";
+  import { useEffect, useState } from "react";
+  // import { fetchCartItems } from "@/store/shop/cart-slice";
+  import { Label } from "../ui/label";
 
 
 
@@ -25,7 +39,7 @@ function MenuItems() {
 
         ))}
     </nav>
-);
+    );
 }
 
 function ShoppingHeader() {
@@ -46,20 +60,76 @@ function ShoppingHeader() {
                     </SheetTrigger>
                     <SheetContent side="left" className="w-full max-w-xs">
                         <MenuItems />
-                        {/* <HeaderRightContent /> */}
+                        <HeaderRightContent /> 
                     </SheetContent>
                 </Sheet>
                 <div className="hidden lg:block">
                     <MenuItems />
                 </div>
 
-                {/* <div className="hidden lg:block">
+                <div className="hidden lg:block">
                     <HeaderRightContent />
-                </div> */}
+                </div>
 
             </div>
         </header>
     );
+}
+
+function HeaderRightContent() {
+    const { user } = useSelector((state) => state.auth);
+    //const { cartItems } = useSelector((state) => state.shopCart);
+    const [openCartSheet, setOpenCartSheet] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    function handleLogout() {
+        dispatch(logoutUser());
+      }
+
+    
+    return (
+        <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+            <Button
+                onClick={() => setOpenCartSheet(true)}
+                variant="outline"
+                size="icon"
+                className="relative"
+            >
+                <ShoppingCart className="w-6 h-6" />
+                {/* <span className="absolute top-[-5px] right-[2px] font-bold text-sm">
+            {cartItems?.items?.length || 0}
+          </span> */}
+                <span className="sr-only">User cart</span>
+            </Button>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Avatar className="bg-black">
+                        <AvatarFallback className="bg-black text-white font-extrabold">
+                            {user?.userName[0].toUpperCase()} 
+                        </AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="right" className="w-56">
+                    <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={() => navigate("/shop/account")}>
+                        <UserCog className="mr-2 h-4 w-4" />
+                        Account
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem onClick={handleLogout}>
+                        <LogOut className="mr-2 h-4 w-4" />
+                        Logout
+                    </DropdownMenuItem>
+                </DropdownMenuContent>   
+            </DropdownMenu>
+        </div>
+    )
+
+
+
+
 }
 
 export default ShoppingHeader;
